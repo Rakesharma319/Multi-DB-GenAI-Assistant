8639dfb4e23faa4ca62322fda2a28981cb563e2b
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-14 19:23:32 +0530

    Update demo111.py

--- demo111.py
+++ demo111.py
@@ -10,7 +10,7 @@ genai.configure(api_key=os.environ["API_KEY"])
 
 
 model = genai.GenerativeModel('gemini-1.5-flash')
-
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 user_input = st.chat_input("Say something")
 
 response = model.generate_content(user_input)
