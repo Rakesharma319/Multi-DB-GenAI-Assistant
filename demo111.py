286590ccf31e37030f0c58fc2bc2beb6a34294ea
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-14 18:10:14 +0530

    Update demo111.py

--- demo111.py
+++ demo111.py
@@ -1,6 +1,12 @@
 import os
 import streamlit as st
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # This line brings all environment variables from .env into os.environ
+print(os.environ['VONAGE_API'])
+
 st.title("This is a title")
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 st.write("Hello, *World!* :sunglasses:")
