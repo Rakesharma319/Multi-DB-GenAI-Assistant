9e762ebd64ce3e5336cc1f8c3a47a9b028477b13
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-14 19:24:58 +0530

    Update demo111.py

--- demo111.py
+++ demo111.py
@@ -1,15 +1,11 @@
 import os
 import streamlit as st
-
-GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
-
 import google.generativeai as genai
-import os
 
-genai.configure(api_key=os.environ["API_KEY"])
-
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 model = genai.GenerativeModel('gemini-1.5-flash')
+
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 user_input = st.chat_input("Say something")
 
