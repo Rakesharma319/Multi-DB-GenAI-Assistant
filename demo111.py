32c5fd4f154424e4df17184585a39e1210beafd0
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-14 18:10:27 +0530

    Update demo111.py

--- demo111.py
+++ demo111.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()  # This line brings all environment variables from .env into os.environ
-print(os.environ['VONAGE_API'])
+print(os.environ['GOOGLE_API_KEY'])
 
 st.title("This is a title")
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
