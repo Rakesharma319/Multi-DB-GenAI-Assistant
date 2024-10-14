35cb6e75205e09a8b51b612d9675cbc6dd2949d0
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-14 19:02:20 +0530

    Update demo111.py

--- demo111.py
+++ demo111.py
@@ -1,10 +1,6 @@
 import os
 import streamlit as st
 
-from dotenv import load_dotenv
-import os
-
-load_dotenv()  # This line brings all environment variables from .env into os.environ
 print(os.environ['GOOGLE_API_KEY'])
 
 st.title("This is a title")
