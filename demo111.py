1311d0745685d4131584645aed62807382b2faa4
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-14 19:19:28 +0530

    Update demo111.py

--- demo111.py
+++ demo111.py
@@ -3,3 +3,24 @@ import streamlit as st
 
 print(os.environ['GOOGLE_API_KEY'])
 st.write(os.environ['GOOGLE_API_KEY'])
+
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+user_input = st.chat_input("Say something")
+
+response = model.generate_content(user_input)
+
+st.write(to_markdown(response.text))
