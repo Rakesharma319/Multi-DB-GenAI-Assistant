092b1243c968f7e4f32612e801f620cc82caffe2
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-14 17:52:29 +0530

    Update demo111.py

--- demo111.py
+++ demo111.py
@@ -1,3 +1,7 @@
 import os
+import streamlit as st
 
-print(os.env(GOOGLE_API_KEY))
+st.title("This is a title")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+st.write("Hello, *World!* :sunglasses:")
+st.write(os.env(GOOGLE_API_KEY))
