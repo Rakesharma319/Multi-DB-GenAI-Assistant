bc8b63b83f5d8f8380bebecbacbfc7cbe8721630
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-15 10:37:51 +0530

    Update demo111.py

--- demo111.py
+++ demo111.py
@@ -2,14 +2,27 @@ import os
 import streamlit as st
 import google.generativeai as genai
 
-GOOGLE_API_KEY1 = st.chat_input("Paste Google API Key")
+google_api_key = st.sidebar.text_input('Google API Key', type='password')
 
-genai.configure(api_key=GOOGLE_API_KEY1)
+genai.configure(api_key=google_api_key)
+
+max_response_tokens = 1500
+token_limit = 6000
+temperature = 0.2
+
+Enter = "False"
+def runapp():
+    Enter = "True"
+
+st.markdown(
+    """# **Database OpenAI Assistant**
+This is an experimental assistant that requires OpenAI access. The app demonstrates the use of OpenAI to support getting insights from Database by just asking questions. The assistant can also generate SQL and Python code for the Questions.
+"""
+)
+
 
 model = genai.GenerativeModel('gemini-1.5-flash')
-
-st.title("_Streamlit_ is :blue[cool] :sunglasses:")
-user_input = st.chat_input("Say something")
+user_input = st.sidebar.text_area("Ask me a question")
 
 if user_input:
   response = model.generate_content(user_input)
