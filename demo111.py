bf8ed3eb3ef4b33d1ef04dd0c0ea826fb74698fc
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-15 10:44:04 +0530

    Update demo111.py

--- demo111.py
+++ demo111.py
@@ -25,6 +25,8 @@ user_input = st.sidebar.text_area("Ask me a question")
 
 if user_input:
   response = model.generate_content(user_input)
+  st.write("Question: "{user_input})
+  st.write("Model Response: ")
   st.write(response.text)
 else:
   st.write("Pending")
