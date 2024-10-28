5200c7f0069682ffe40803bf4a3a97c02be6f1da
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-28 20:20:44 +0530

    Create GraphDB_GenAI_Assistant.py

---
+++ GraphDB_GenAI_Assistant.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import os
+
+st.write("The current movie title is", title)
+
+NEO4J_URI = st.text_input("Enter NEO4J_URI",key="placeholder")
+NEO4J_USERNAME = st.text_input("Enter NEO4J_USERNAME",key="placeholder")
+NEO4J_PASSWORD = st.text_input("Enter NEO4J_PASSWORD",key="placeholder",type="password")
+
+st.write(NEO4J_URI)
+st.write(NEO4J_USERNAME)
+
+
+os.environ["NEO4J_URI"]=NEO4J_URI
+os.environ["NEO4J_USERNAME"]=NEO4J_USERNAME
+os.environ["NEO4J_PASSWORD"]=NEO4J_PASSWORD
+
+from langchain_community.graphs import Neo4jGraph
+
+graph=Neo4jGraph(
+    url=NEO4J_URI,
+    username=NEO4J_USERNAME,
+    password=NEO4J_PASSWORD,
+)
+
+schema = graph.schema
+
+if schema:
+  st.write(schema)
+else:
+  st.write("Check DB Connection")
+
