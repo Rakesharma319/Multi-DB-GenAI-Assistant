7f9a427a767c5261e1ba0763b56d4b2fc49dbc3e
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-28 20:33:33 +0530

    Update GraphDB_GenAI_Assistant.py

--- GraphDB_GenAI_Assistant.py
+++ GraphDB_GenAI_Assistant.py
@@ -3,8 +3,6 @@ import os
 
 st.title("🦜🔗 Quickstart App")
 
-submitted = False
-
 NEO4J_URI = st.text_input("Enter NEO4J_URI",key="URI")
 NEO4J_USERNAME = st.text_input("Enter NEO4J_USERNAME",key="Username")
 NEO4J_PASSWORD = st.text_input("Enter NEO4J_PASSWORD",key="placeholder",type="password")
@@ -18,7 +16,7 @@ os.environ["NEO4J_PASSWORD"]=NEO4J_PASSWORD
 
 submitted = st.form_submit_button("Submit")
 
-if submitted:
+if st.button("Login"):
     from langchain_community.graphs import Neo4jGraph
     graph=Neo4jGraph(
         url=NEO4J_URI,
