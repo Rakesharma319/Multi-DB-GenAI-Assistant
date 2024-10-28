5ab0905ee46ed60d8fe61b63ba50b7d7338fd6ac
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-28 20:30:29 +0530

    Update GraphDB_GenAI_Assistant.py

--- GraphDB_GenAI_Assistant.py
+++ GraphDB_GenAI_Assistant.py
@@ -3,6 +3,8 @@ import os
 
 st.title("🦜🔗 Quickstart App")
 
+submitted = False
+
 NEO4J_URI = st.text_input("Enter NEO4J_URI",key="URI")
 NEO4J_USERNAME = st.text_input("Enter NEO4J_USERNAME",key="Username")
 NEO4J_PASSWORD = st.text_input("Enter NEO4J_PASSWORD",key="placeholder",type="password")
@@ -10,23 +12,24 @@ NEO4J_PASSWORD = st.text_input("Enter NEO4J_PASSWORD",key="placeholder",type="pa
 st.write(NEO4J_URI)
 st.write(NEO4J_USERNAME)
 
-
 os.environ["NEO4J_URI"]=NEO4J_URI
 os.environ["NEO4J_USERNAME"]=NEO4J_USERNAME
 os.environ["NEO4J_PASSWORD"]=NEO4J_PASSWORD
 
-from langchain_community.graphs import Neo4jGraph
+submitted = st.form_submit_button("Submit")
 
-graph=Neo4jGraph(
-    url=NEO4J_URI,
-    username=NEO4J_USERNAME,
-    password=NEO4J_PASSWORD,
-)
+if submitted:
+    from langchain_community.graphs import Neo4jGraph
 
-schema = graph.schema
-
-if schema:
-  st.write(schema)
-else:
-  st.write("Check DB Connection")
-
+    graph=Neo4jGraph(
+        url=NEO4J_URI,
+        username=NEO4J_USERNAME,
+        password=NEO4J_PASSWORD,
+    )
+    
+    schema = graph.schema
+    
+    if schema:
+    st.write(schema)
+    else:
+    st.write("Check DB Connection")
