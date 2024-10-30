64e6971979f955da9c001d68af73bd3990525157
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-30 14:26:04 +0530

    Update GraphDB_GenAI_Assistant.py

--- GraphDB_GenAI_Assistant.py
+++ GraphDB_GenAI_Assistant.py
@@ -69,9 +69,9 @@ def display_output(responce):
     if "Answer" in action3:
       print(action3)
 
-# os.environ["NEO4J_URI"]=NEO4J_URI
-# os.environ["NEO4J_USERNAME"]=NEO4J_USERNAME
-# os.environ["NEO4J_PASSWORD"]=NEO4J_PASSWORD
+os.environ["NEO4J_URI"]=NEO4J_URI
+os.environ["NEO4J_USERNAME"]=NEO4J_USERNAME
+os.environ["NEO4J_PASSWORD"]=NEO4J_PASSWORD
 
 
   
