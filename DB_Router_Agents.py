9af8c0b7e776a8ce3fc71aaf30fb68d38739ea96
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-30 18:18:26 +0530

    Update DB_Router_Agents.py

--- DB_Router_Agents.py
+++ DB_Router_Agents.py
@@ -1,28 +1,26 @@
+import streamlit as st
 from RDBMS_GenAI_Assistant import rdbms_main
 from GraphDB_GenAI_Assistant import graphDB_main
-import streamlit as st
 
+# Streamlit app
 st.title("🦜🔗 Database Router Agents GenAI Assistant")
-col1, col2 = st.columns(2)
 
-with col1:
-  genre = st.radio(
-    "Choose Databse to Chat",
-    ["***Relational_Database***", "***Graph_Database***"],
-    captions=[
-        "Relational DB contains Music Related data.",
-        "Graph DB contains Movie related data.",
-    ],
-  )
+# User input for name
+name = st.text_input("Enter your DB")
 
-with col2:
-  if genre == "***Relational_Database***":
-    st.write("You selected Relational Database.")
-    rdbms_main()
-    
-  elif genre == "***Graph_Database***":
-    st.write("You selected Graph Database.")
-    graphDB_main()
-    
-  else:
-    st.write("Not yet Implemented.")
+# Dropdown for function selection
+function_choice = st.selectbox("Choose a function to call", ["Relational_Database", "Graph_Database"])
+
+# Button to call the function
+if st.button("Call Function"):
+    if function_choice == "Relational_Database":
+        rdbms_main()
+    elif function_choice == "Graph_Database":
+        graphDB_main()
+    else:
+        result = "Invalid choice"
+
+
+# Run the Streamlit app
+if __name__ == "__main__":
+    st.run()
