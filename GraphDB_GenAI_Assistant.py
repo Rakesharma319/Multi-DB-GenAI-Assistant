427efbde74d315bc8dd4113732f4985e9cd23ddf
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-30 13:17:55 +0530

    Update GraphDB_GenAI_Assistant.py

--- GraphDB_GenAI_Assistant.py
+++ GraphDB_GenAI_Assistant.py
@@ -136,78 +136,17 @@ if st.button("Login"):
         password=st.session_state.NEO4J_PASSWORD,
     )
     
-    st.session_state.schema = st.session_state.graph.schema
+st.session_state.schema = st.session_state.graph.schema
     
-    if st.session_state.schema:
-        st.write("Database Connection Success!!")
-        st.session_state.question=st.text_input("Ask Question")
-        if st.session_state.question:
-          # CYPHER_GENERATION_TEMPLATE = f"""
-          # You are a smart AI assistant to help answer business questions based on analyzing data.
-          # You can plan solving the question with one or multiple thought step. 
-          # At each thought step, you can write python code to analyze data to assist you. Observe what you get at each step to plan for the next step.
-          
-          # Here is the user question: {st.session_state.question}
-          
-          # You are given following utilities to help you retrieve data and communicate your result to end user.
-          # 1. execute_cypher_query(cypher_query: str): A Python function can query data from the Neo4j given a query which you need to create. 
-          # The query has to be syntactically correct for Neo4j and Use only the provided relationship types and properties in the schema.
-          # Use alias when using the WITH keyword in cypher query.
-          # Do not use any other relationship types or properties that are not provided.
-          # Schema:{st.session_state.schema}. The execute_cypher_query function returns a Python pandas dataframe contain the results of the query.
-          # 2. Use plotly library for data visualization.
-          #     - When ever you try to read column data from the panadas dataframe, use column name as m.col_name instead only col_name.
-          #     - Example: fig = px.bar(step1_df, x="m.title", y="m.imdbRating", title="Top 10 Movies by IMDb Ratings")
-          # 3. Use observe(label: str, data: any) utility function to observe data under the label for your evaluation. 
-          # Use observe() function instead of print() as this is executed in streamlit environment. 
-          # Due to system limitation, you will only see the first 10 rows of the dataset.
-          # 4. To communicate with user, use show() function on data, text and plotly figure. show() is a utility function that can render different types of data to end user. Remember, you don't see data with show(), only user does. You see data with observe()
-          #     - If you want to show  user a plotly visualization, then use ```show(fig)```
-          #     - If you want to show user data which is a text or a pandas dataframe or a list, use ```show(data)```
-          #     - Never use print(). User don't see anything with print()
-          # 5. Lastly, don't forget to deal with data quality problem. You should apply data imputation technique to deal with missing data or NAN data.
-          # 6. Always follow the flow of Thought: , Observation:, Action: and Answer: as in template below strictly.
-          
-          # <<Template>>
-          # Question: User Question
-          # Thought 1: Your thought here.
-          # Action:
-          # ```python
-          # #Import neccessary libraries here
-          # import numpy as np
-          # #Query some data
-          # cypher_query = "SOME CYPHER QUERY"
-          # step1_df = execute_cypher_query(cypher_query)
-          # # Replace 0 with NaN. Always have this step
-          # step1_df['Some_Column'] = step1_df['Some_Column'].replace(0, np.nan)
-          
-          # #observe query result
-          # ```
-          # Observation:
-          # step1_df is displayed here
-          # Thought 2: Your thought here
-          # Action:
-          # ```python
-          # import plotly.express as px
-          # #from step1_df, perform some data analysis action to produce step2_df
-          # #To see the data for yourself the only way is to use observe()
-          # observe("some_label", step2_df) #Always use observe()
-          # #Decide to show it to user, Use alias when using the WITH keyword
-          # fig=px.line(step2_df)
-          # #visualize fig object to user.
-          # show(fig)
-          # #you can also directly display tabular or text data to end user.
-          # show(step2_df)
-          # ```
-          # Observation:
-          # step2_df is displayed here
-          # Answer: Your final answer and comment for the question
-          # <</Template>>
-          # """
-          # if st.button("Enter"):
-          model = genai.GenerativeModel('gemini-1.5-flash')
-          response = model.generate_content(CYPHER_GENERATION_TEMPLATE)
-          llm_response = response.text
-          display_output(llm_response)
-    else:
-        st.write("Check DB Connection")
+if st.session_state.schema:
+    st.write("Database Connection Success!!")
+else:
+    st.write("Check DB Connection")
+    
+st.session_state.question=st.text_input("Ask Question")
+if st.session_state.question:
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    response = model.generate_content(CYPHER_GENERATION_TEMPLATE)
+    llm_response = response.text
+    display_output(llm_response)
+
