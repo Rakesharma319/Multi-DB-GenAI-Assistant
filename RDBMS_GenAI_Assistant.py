3f20c12ba1d666ca1713b37ebcc60b4985dad19c
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-30 17:21:06 +0530

    Update RDBMS_GenAI_Assistant.py

--- RDBMS_GenAI_Assistant.py
+++ RDBMS_GenAI_Assistant.py
@@ -297,21 +297,22 @@ else:
 	
 
 def rdbms_main():
-    n = 0
-	try:
-        response = get_final_output_from_model(prompt_to_get_sqlwitanalysis)
-	except Exception as e:
-        time.sleep(8)  # sleep for 8 seconds
-        while n < 5:
-           	try:
-               	response = get_final_output_from_model(prompt_to_get_sqlwitanalysis)
-           	except Exception as e:
-               	n += 1
-               	print(
-                  		"error calling open AI, I am retrying 5 attempts , attempt ", n
-               	)
-               	time.sleep(8)  # sleep for 8 seconds
-               	print(e)
+	response = get_final_output_from_model(prompt_to_get_sqlwitanalysis)
+ #    n = 0
+	# try:
+ #        response = get_final_output_from_model(prompt_to_get_sqlwitanalysis)
+	# except Exception as e:
+ #        time.sleep(8)  # sleep for 8 seconds
+ #        while n < 5:
+ #           	try:
+ #               	response = get_final_output_from_model(prompt_to_get_sqlwitanalysis)
+ #           	except Exception as e:
+ #               	n += 1
+ #               	print(
+ #                  		"error calling open AI, I am retrying 5 attempts , attempt ", n
+ #               	)
+ #               	time.sleep(8)  # sleep for 8 seconds
+ #               	print(e)
 	
 	display_output(response)
 	
