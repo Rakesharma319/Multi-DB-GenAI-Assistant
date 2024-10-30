452d21e3be674ce6fd51fd2d8b852ed018046e56
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-30 13:22:26 +0530

    Update GraphDB_GenAI_Assistant.py

--- GraphDB_GenAI_Assistant.py
+++ GraphDB_GenAI_Assistant.py
@@ -66,13 +66,13 @@ if st.button("Login"):
     os.environ["NEO4J_USERNAME"]=st.session_state.NEO4J_USERNAME
     os.environ["NEO4J_PASSWORD"]=st.session_state.NEO4J_PASSWORD
     from langchain_community.graphs import Neo4jGraph
-    st.session_state.graph=Neo4jGraph(
+    graph=Neo4jGraph(
         url=st.session_state.NEO4J_URI,
         username=st.session_state.NEO4J_USERNAME,
         password=st.session_state.NEO4J_PASSWORD,
     )
     
-st.session_state.schema = st.session_state.graph.schema
+st.session_state.schema = graph.schema
     
 if st.session_state.schema:
     st.write("Database Connection Success!!")
