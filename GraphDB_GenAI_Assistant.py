84d81495270303cede74943f01d019bb62bb4b3e
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-30 14:32:41 +0530

    Update GraphDB_GenAI_Assistant.py

--- GraphDB_GenAI_Assistant.py
+++ GraphDB_GenAI_Assistant.py
@@ -158,8 +158,8 @@ Answer: Your final answer and comment for the question
 
 st.write(CYPHER_GENERATION_TEMPLATE)
 
-# model = genai.GenerativeModel('gemini-1.5-flash')
-# response = model.generate_content(CYPHER_GENERATION_TEMPLATE)
-# llm_response = response.text
-# display_output(llm_response)
+model = genai.GenerativeModel('gemini-1.5-flash')
+response = model.generate_content(CYPHER_GENERATION_TEMPLATE)
+llm_response = response.text
+display_output(llm_response)
 
