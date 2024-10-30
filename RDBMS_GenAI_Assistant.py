d69164dc09c0a1eefc5c29266b206acc510dc25d
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-30 16:27:08 +0530

    Update RDBMS_GenAI_Assistant.py

--- RDBMS_GenAI_Assistant.py
+++ RDBMS_GenAI_Assistant.py
@@ -298,16 +298,15 @@ def rdbms_main():
         try:
                 response = get_final_output_from_model()
         except Exception as e:
-            time.sleep(8)  # sleep for 8 seconds
-            while n < 5:
-                    try:
-                        response = get_final_output_from_model()
-                        
-                    except Exception as e:
-                        n += 1
-                        print("error calling open AI, I am retrying 5 attempts , attempt ", n)
-                        time.sleep(8)  # sleep for 8 seconds
-                        print(e)
+                time.sleep(8)  # sleep for 8 seconds
+                while n < 5:
+                        try:
+                            response = get_final_output_from_model()
+                        except Exception as e:
+                            n += 1
+                            print("error calling open AI, I am retrying 5 attempts , attempt ", n)
+                            time.sleep(8)  # sleep for 8 seconds
+                            print(e)
         
         display_output(response)
     
