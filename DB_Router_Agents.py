9a193dbf4e2913939e32963a9fc83517ba278404
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-10-30 17:57:41 +0530

    Update DB_Router_Agents.py

--- DB_Router_Agents.py
+++ DB_Router_Agents.py
@@ -2,22 +2,26 @@
 # from GraphDB_GenAI_Assistant import GraphDB_main
 import streamlit as st
 
+col1, col2 = st.columns(2)
+
 st.title("🦜🔗 Database Router Agents GenAI Assistant")
 
-genre = st.radio(
+with col1:
+  genre = st.radio(
     "Choose Databse to Chat",
     ["***Relational_Database***", "***Graph_Database***"],
     captions=[
         "Relational DB contains Music Related data.",
         "Graph DB contains Movie related data.",
     ],
-)
+  )
 
-if genre == "Relational_Database":
+with col2:
+  if genre == "Relational_Database":
     st.write("You selected Relational Database.")
     # rdbms_main()
-elif genre == "Graph_Database":
+  elif genre == "Graph_Database":
     st.write("You selected Graph Database.")
     # GraphDB_main()
-else:
+  else:
     st.write("Not yet Implemented.")
