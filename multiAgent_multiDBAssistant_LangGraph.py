02b511154b753c46a4adacf67c35e41e3ca2003b
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-11-06 14:11:13 +0530

    Update multiAgent_multiDBAssistant_LangGraph.py

--- multiAgent_multiDBAssistant_LangGraph.py
+++ multiAgent_multiDBAssistant_LangGraph.py
@@ -27,7 +27,7 @@ question2 = "Who is Virat Kohali?" #-- wiki
 question3 = "visualize all albums by month?" # -- rdbms
 question4 = "write a quote to become richer, and aslo show tags and author?" #--- vector
 
-question = st.sidebar.text_input('select question',(question1,question2,question3,question4))
+question = st.sidebar.selectbox('select question',(question1,question2,question3,question4))
 
 ## --------- Tools Code start -----
 
