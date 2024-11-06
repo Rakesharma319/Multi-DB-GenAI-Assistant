cc78b4427538f9089fe5ab4da029b5b264f29e50
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-11-06 14:23:31 +0530

    Update multiAgent_multiDBAssistant_LangGraph.py

--- multiAgent_multiDBAssistant_LangGraph.py
+++ multiAgent_multiDBAssistant_LangGraph.py
@@ -27,7 +27,7 @@ question2 = "Who is Virat Kohali?" #-- wiki
 question3 = "visualize all albums by month?" # -- rdbms
 question4 = "write a quote to become richer, and aslo show tags and author?" #--- vector
 
-question = st.selectbox(
+option = st.selectbox(
     "How would you like to be contacted?",
     (question1,question2,question3,question4),
 )
